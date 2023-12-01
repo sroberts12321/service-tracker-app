a008ce5407f63e0951541d360ed10b60d5ae4a12
Stephen Roberts <Sroberts12321@gmail.com>
2023-12-01 15:04:23 -0600

    Removed comments

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,8 +20,6 @@ import {
     orderBy,
     CollectionReference 
 } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
