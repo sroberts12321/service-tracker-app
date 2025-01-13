19242b9e124286d4ddaa1208db5b8a28af5cdfa6
Stephen Roberts <Sroberts12321@gmail.com>
2025-01-12 22:07:05 -0600

    Added phone number to possible search terms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -114,7 +114,7 @@
 	$: if (customerStore.length > 0) {
 		const searchCustomers = customerStore.map((customer) => ({
 			...customer,
-			searchTerms: `${customer.lastName} ${customer.firstName} ${customer.email} ${customer.nickname}`
+			searchTerms: `${customer.lastName} ${customer.firstName} ${customer.email} ${customer.nickname} ${customer.phone}`
 		}));
 		searchStore = createSearchStore(searchCustomers);
 		const unsubscribe = searchStore.subscribe((model) => searchHandler(model));
