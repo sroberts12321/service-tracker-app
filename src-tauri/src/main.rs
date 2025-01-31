c02afdc4f5b625c01cc2e20c53da29a876ce2fb5
Stephen Roberts <Sroberts12321@gmail.com>
2025-01-30 22:07:55 -0600

    Tauri v2 Upgrade file changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,6 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    app_lib::run();
 }
