0c7b81c181fb79eefcb4bf62fd65b1fd5cbfa733
Stephen Roberts <Sroberts12321@gmail.com>
2025-02-12 21:28:29 -0600

    Tauri v2 upgrade changes, notification changes, formatting

--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,37 +1,36 @@
 export class Service {
-    customerId: string;
-    id: string;
-    dropOffDate: string;
-    paid: boolean;
-    pickedUp: boolean;
-    pickUpDate: string;
-    isReady: boolean;
-    referenceNum: string;
-    typeOfService: string;
-    notes: string;
+	customerId: string;
+	id: string;
+	dropOffDate: string;
+	paid: boolean;
+	pickedUp: boolean;
+	pickUpDate: string;
+	isReady: boolean;
+	referenceNum: string;
+	typeOfService: string;
+	notes: string;
 
-    constructor
-    (
-        customerId: string,
-        id: string, 
-        dropOffDate: string, 
-        paid: boolean, 
-        pickedUp: boolean, 
-        pickUpDate: string, 
-        isReady: boolean, 
-        referenceNum: string, 
-        typeOfService: string, 
-        notes: string
-    ) {
-        this.customerId = customerId;
-        this.id = id;
-        this.dropOffDate = dropOffDate;
-        this.paid = paid;
-        this.pickedUp = pickedUp;
-        this.pickUpDate = pickUpDate;
-        this.isReady = isReady;
-        this.referenceNum = referenceNum;
-        this.typeOfService = typeOfService;
-        this.notes = notes;
-    }
+	constructor(
+		customerId: string,
+		id: string,
+		dropOffDate: string,
+		paid: boolean,
+		pickedUp: boolean,
+		pickUpDate: string,
+		isReady: boolean,
+		referenceNum: string,
+		typeOfService: string,
+		notes: string
+	) {
+		this.customerId = customerId;
+		this.id = id;
+		this.dropOffDate = dropOffDate;
+		this.paid = paid;
+		this.pickedUp = pickedUp;
+		this.pickUpDate = pickUpDate;
+		this.isReady = isReady;
+		this.referenceNum = referenceNum;
+		this.typeOfService = typeOfService;
+		this.notes = notes;
+	}
 }
--- src/lib/stores/notifications.ts
+++ src/lib/stores/notifications.ts
@@ -1,47 +1,47 @@
-import { writable, derived } from "svelte/store"
+import { writable, derived } from 'svelte/store';
 
-const TIMEOUT = 3000
+const TIMEOUT = 3000;
 
-function createNotificationStore (timeout: any) {
-    const _notifications = writable([])
+function createNotificationStore(timeout: number) {
+	const _notifications = writable([]);
 
-    function send (message: String, type = "default", timeout: any) {
-        _notifications.update(state => {
-            return [...state, { id: id(), type, message, timeout }]
-        })
-    }
+	function send(message: string, type = 'default', timeout: number) {
+		_notifications.update((state) => {
+			return [...state, { id: id(), type, message, timeout }];
+		});
+	}
 
-    let timers = []
+	let timers = [];
 
-    const notifications = derived(_notifications, ($_notifications, set) => {
-        set($_notifications)
-        if ($_notifications.length > 0) {
-            const timer = setTimeout(() => {
-                _notifications.update(state => {
-                    state.shift()
-                    return state
-                })
-            }, $_notifications[0].timeout)
-            return () => {
-                clearTimeout(timer)
-            }
-        }
-    })
-    const { subscribe } = notifications
+	const notifications = derived(_notifications, ($_notifications, set) => {
+		set($_notifications);
+		if ($_notifications.length > 0) {
+			const timer = setTimeout(() => {
+				_notifications.update((state) => {
+					state.shift();
+					return state;
+				});
+			}, $_notifications[0].timeout);
+			return () => {
+				clearTimeout(timer);
+			};
+		}
+	});
+	const { subscribe } = notifications;
 
-    return {
-        subscribe,
-        send,
-        default: (msg: String, timeout: any) => send(msg, "default", timeout),
-        danger: (msg: String, timeout: any) => send(msg, "danger", timeout),
-        warning: (msg: String, timeout: any) => send(msg, "warning", timeout),
-        info: (msg:String, timeout: any) => send(msg, "info", timeout),
-        success: (msg: String, timeout: any) => send(msg, "success", timeout),
-    }
+	return {
+		subscribe,
+		send,
+		default: (msg: string, timeout: number) => send(msg, 'default', timeout),
+		danger: (msg: string, timeout: number) => send(msg, 'danger', timeout),
+		warning: (msg: string, timeout: number) => send(msg, 'warning', timeout),
+		info: (msg: string, timeout: number) => send(msg, 'info', timeout),
+		success: (msg: string, timeout: number) => send(msg, 'success', timeout)
+	};
 }
 
 function id() {
-    return '_' + Math.random().toString(36).substr(2, 9);
-};
+	return '_' + Math.random().toString(36).substr(2, 9);
+}
 
-export const notifications = createNotificationStore(TIMEOUT)
+export const notifications = createNotificationStore(TIMEOUT);
