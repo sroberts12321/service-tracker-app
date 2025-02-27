4de06bec1caeab80429b7567953c7891164de390
Stephen Roberts <Sroberts12321@gmail.com>
2025-02-27 12:54:12 -0600

    Icon update, config updated, launch prep

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_store::Builder::new().build())
-        .setup(|app| {
-            #[cfg(desktop)]
-            Ok(())
-        })
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .run(tauri::generate_context!())
